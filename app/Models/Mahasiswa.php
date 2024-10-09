3f464392a3591f37a3f1808c957b2d50d08a42d0
Gustian3051 <gustianprayoga31@gmail.com>
2024-10-09 15:12:17 +0700

    update-mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -5,13 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Model;
+use Illuminate\Support\Facades\Hash;
 
 class Mahasiswa extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nama', 'nim', 'kelas_id', 'email', 'password', 'foto'];
-    protected $hidden = ['password'];
+    protected static function booted() {
+        static::creating(function ($mahasiswa) {
+            $mahasiswa->password = Hash::make('@Poli' . $mahasiswa->nim);
+    });
+    }
 
 
     public function kelas()
